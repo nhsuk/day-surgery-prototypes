a4bc86b161f4280fc7c8d8d1ec57120e9e264911
Tim Allan <tim.allan@digital.hmrc.gov.uk>
2016-10-03 18:00:23 +0100

    integrated query and hacked progressive reveal JS

---
+++ assets/js/application.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function() {
+
+// console.log('hello world');
+
+$('.hidden-help').hide();
+
+// visibility toggle
+(function($) {
+    $.fn.invisible = function() {
+        return this.css("visibility", "hidden");
+    };
+    $.fn.visible = function() {
+        return this.css("visibility", "visible");
+    };
+})(jQuery);
+
+  // Example - Highlight grid
+
+  if ($('.js-highlight-grid').length>0) {
+
+    $('.js-highlight-grid').click(function(e) {
+
+      e.preventDefault();
+      var html = $('html');
+
+      if ($('.is-inner-block-highlight').length>0) {
+        // Don't add more than once
+      } else {
+        $('.grid .inner-block').wrapInner('<div class="is-inner-block-highlight"></div>');
+      }
+
+      if (html.hasClass('example-highlight-grid')) {
+          html.removeClass('example-highlight-grid');
+      } else {
+          html.addClass('example-highlight-grid');
+      }
+
+    });
+
+  }
+
+
+});
